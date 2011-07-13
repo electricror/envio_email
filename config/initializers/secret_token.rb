c3e30a74fbcf6f29cd60d144f8157cc4054f0ce7
Vitor Santos <vitorfran@gmail.com>
2011-07-12 22:20:47 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnvioEmail::Application.config.secret_token = '773cb58874eebeeea19938dca4109808986c19f7f6092114e23b2111696b842bc7c381f1778f0ceb8bc9df9ce1f22e01daeade8b27d5141db74ca818e1770e56'
